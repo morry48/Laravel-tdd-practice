99786e2f5a7a9ea3d0c306acbeffb3425d879ceb
morry48 <yyyyuuuuuuuu0408@gmail.com>
2020-12-20 01:39:51 +0900

    remainingCount()の実装

--- app/Models/Lesson.php
+++ app/Models/Lesson.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Lesson extends Model
 {
@@ -13,6 +14,11 @@ class Lesson extends Model
 
     private function remainingCount(): int
     {
-        return 0;
+        return $this->capacity - $this->reservations()->count();
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
