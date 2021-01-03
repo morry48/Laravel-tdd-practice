faa5ac8a27b75a2599fe846cebc62a02df487bcf
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-01-03 23:00:56 +0900

    3-6 メール課題のテスト実装

--- tests/Feature/Http/Controllers/Lesson/ReserveControllerTest.php
+++ tests/Feature/Http/Controllers/Lesson/ReserveControllerTest.php
@@ -3,10 +3,14 @@
 namespace Tests\Feature\Http\Controllers\Lesson;
 
 use App\Models\Lesson;
+use App\Models\Reservation;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Http\Response;
 use Tests\Factories\Traits\CreatesUser;
 use Tests\TestCase;
+use App\Notifications\ReservationCompleted;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Notifications\Notification as IlluminateNotification;
 
 class ReserveControllerTest extends TestCase
 {
@@ -15,6 +19,7 @@ class ReserveControllerTest extends TestCase
 
     public function testInvoke()
     {
+        Notification::fake();
         $lesson = factory(Lesson::class)->create();
         $user = $this->createUser();
         $this->actingAs($user);
@@ -27,5 +32,40 @@ class ReserveControllerTest extends TestCase
             'lesson_id' => $lesson->id,
             'user_id' => $user->id,
         ]);
+
+        Notification::assertSentTo(
+            $user,
+            ReservationCompleted::class,
+            function (ReservationCompleted $notification) use ($lesson) {
+                return $notification->lesson->id === $lesson->id;
+            }
+        );
+    }
+
+    public function testInvoke_異常系()
+    {
+        Notification::fake();
+        $lesson = factory(Lesson::class)->create(['capacity' => 1]);
+        $anotherUser = $this->createUser();
+        $lesson->reservations()->save(factory(Reservation::class)->make(['user_id' => $anotherUser->id]));
+
+        $user = $this->createUser();
+        $this->actingAs($user);
+
+        $response = $this->from("/lessons/{$lesson->id}")
+            ->post("/lessons/{$lesson->id}/reserve");
+
+        $response->assertStatus(Response::HTTP_FOUND);
+        $response->assertRedirect("/lessons/{$lesson->id}");
+        $response->assertSessionHasErrors();
+        // メッセージの中身まで確認したい場合は以下の2行も追加
+        $error = session('errors')->first();
+        $this->assertStringContainsString('予約できません。', $error);
+
+        $this->assertDatabaseMissing('reservations', [
+            'lesson_id' => $lesson->id,
+            'user_id' => $user->id,
+        ]);
+        Notification::assertNotSentTo($user, ReservationCompleted::class);
     }
 }
