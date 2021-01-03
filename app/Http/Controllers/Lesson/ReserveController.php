1827ffed981d6660977e040ee92ce1f072502d8a
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-01-03 22:27:23 +0900

    3-4 Reserveの正常系テスト

---
+++ app/Http/Controllers/Lesson/ReserveController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Lesson;
+
+use App\Http\Controllers\Controller;
+use App\Models\Lesson;
+use App\Models\Reservation;
+use Illuminate\Support\Facades\Auth;
+
+class ReserveController extends Controller
+{
+    public function __invoke(Lesson $lesson)
+    {
+        $user = Auth::user();
+        Reservation::create(['lesson_id' => $lesson->id, 'user_id' => $user->id]);
+        return redirect()->route('lessons.show', ['lesson' => $lesson]);
+    }
+}
