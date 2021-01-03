e998d4e8d0b5d9d2d85224a0e28e3203259cbcf6
morry48 <yyyyuuuuuuuu0408@gmail.com>
2021-01-03 21:36:41 +0900

    3-1認証のテスト

--- resources/views/lesson/show.blade.php
+++ resources/views/lesson/show.blade.php
@@ -1,12 +1,13 @@
 <h1>{{ $lesson->name }}</h1>
 <div>
   <span>空き状況: {{ $lesson->vacancyLevel }}</span>
-    <div>
-    @if($lesson->vacancyLevel->mark() === '◎' || $lesson->vacancyLevel->mark() === '△')
-        <button class="btn btn-primary">このレッスンを予約する</button>
-    @else
-        <span class="btn btn-primary disabled">予約できません</span>
-    @endif
-    </div>
+</div>
+
+<div>
+@can('reserve', $lesson)
+    <button class="btn btn-primary">このレッスンを予約する</button>
+@else
+    <span class="btn btn-primary disabled">予約できません</span>
+@endcan
 </div>
 
